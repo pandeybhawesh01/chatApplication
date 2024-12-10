e7aff00f70245f5af8b48dcd9418e3b447ad81fe
pandeybhawesh01 <pbhaweshpandey001@gmail.com>
2024-12-10 15:00:22 +0530

    deplyoment commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from 'express';
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
@@ -13,18 +14,29 @@ dotenv.config();
 
 const Port = process.env.Port || 5000;
 
+const __dirname = path.resolve();
+
 
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) =>{
-    res.send("<H1>Hello World</H1>");
-})
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+console.log("Static files served from:", path.join(__dirname, "/frontend/dist"));
+
+// app.get("/", (req, res) =>{
+//     res.send("<H1>Hello World</H1>");
+// })
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+
+
+app.get("*", (req, res) =>{
+    res.sendFile(path.join(__dirname, "frontend", "dist","index.html"));
+})
 
 
 server.listen(Port,() => {
