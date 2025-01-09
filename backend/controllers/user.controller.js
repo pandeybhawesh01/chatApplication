700f5a7dae7946b8b291f199843b4da92a293767
Bhawesh Pandey <153444734+pandeybhawesh01@users.noreply.github.com>
2025-01-09 19:15:15 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,12 +2,14 @@ import User from "../models/user.models.js";
 
 export const getUserForSidebar = async (req, res) => {
     try {
+        console.log("ok");
         const loogedInUserId = req.user._id
 
         const filteredUsers = await User.find({_id: {$ne: loogedInUserId}}).select("-password");
 
         res.status(200).json(filteredUsers)
     } catch (error) {
+        console.log("no");
         console.log("Error in getUserForSidebar: " ,error.message);
         res.status(500).json({error: "Internal server error"});
     }
